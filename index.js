788fe4868de9672f2b091bf43ca808a681e5b42a
Jannis R <mail@jannisr.de>
2019-07-28 19:47:49 +0200

    WIP: new UI

--- index.js
+++ index.js
@@ -4,32 +4,7 @@ const AirPlay = require('airplay-protocol')
 
 const play = (mediaUrl, address, cb) => {
 	const device = new AirPlay(address)
-
-	const getInfo = () => {
-		device.playbackInfo((err, res, playbackInfo) => {
-			setTimeout(getInfo, 2000)
-
-			if (err) return device.emit('error', err)
-			device.emit('playbackInfo', playbackInfo)
-		})
-	}
-
-	device.play(mediaUrl, (err) => {
-		if (err) return cb(err)
-
-		const waitForPlay = () => {
-			device.playbackInfo((err, res, playbackInfo) => {
-				if (err) return cb(err)
-
-				device.emit('playbackInfo', playbackInfo)
-				if (playbackInfo && playbackInfo.readyToPlay) {
-					getInfo()
-					cb()
-				} else setTimeout(waitForPlay, 500)
-			})
-		}
-		waitForPlay()
-	})
+	device.play(mediaUrl, cb)
 
 	return device
 }
