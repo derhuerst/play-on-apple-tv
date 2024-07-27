fa67763957ae0f5175709b43e39ae6015f51cf18
Jannis R <mail@jannisr.de>
2024-07-27 19:03:22 +0200

    add linter, let CI run it 💚

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js'
+import globals from 'globals'
+
+export default [
+	eslint.configs.recommended,
+	{
+		languageOptions: {
+			ecmaVersion: 2022,
+			globals: globals.node,
+		},
+		rules: {
+			'no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					args: 'none',
+					ignoreRestSiblings: false
+				},
+			],
+		},
+	},
+]
